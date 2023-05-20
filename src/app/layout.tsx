52b6d6ff2e2708e9f6db763217d32abe1cc23d2e
daveg7lee <daveg7lee@gmail.com>
2023-05-20 18:27:00 +0900

    🐛 use devnet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,22 +21,22 @@ export default function RootLayout({
           authConfig={{
             domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN ?? "",
           }}
-          // activeChain={{
-          //   chainId: 2000777,
-          //   rpc: [
-          //     "http://aops-custom-202305-2crvsg-nlb-1d600174371701f9.elb.ap-northeast-2.amazonaws.com:9650/ext/bc/XpX1yGquejU5cma1qERzkHKDh4fsPKs4NttnS1tErigPzugx5/rpc",
-          //   ],
-          //   nativeCurrency: {
-          //     decimals: 18,
-          //     name: "Avalanche coin",
-          //     symbol: "AVAX",
-          //   },
-          //   shortName: "AVAX",
-          //   slug: "AVAX",
-          //   testnet: true,
-          //   chain: "Glitch",
-          //   name: "Glitch devnet",
-          // }}
+          activeChain={{
+            chainId: 2000777,
+            rpc: [
+              "http://aops-custom-202305-2crvsg-nlb-1d600174371701f9.elb.ap-northeast-2.amazonaws.com:9650/ext/bc/XpX1yGquejU5cma1qERzkHKDh4fsPKs4NttnS1tErigPzugx5/rpc",
+            ],
+            nativeCurrency: {
+              decimals: 18,
+              name: "Avalanche coin",
+              symbol: "AVAX",
+            },
+            shortName: "AVAX",
+            slug: "AVAX",
+            testnet: true,
+            chain: "Glitch",
+            name: "Glitch devnet",
+          }}
         >
           <RecoilRoot>
             <Providers>
